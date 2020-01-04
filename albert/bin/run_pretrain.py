2d882093bdf2d60d345aefad195e17598276286a
DatNgo <dat821168@gmail.com>
2020-01-04 10:30:16 +0700

    Commit containt:     _Update config object for pretraing.     _Update Tokenization.
    Time: 10:30 04/01/2020

---
+++ albert/bin/run_pretrain.py
@@ -0,0 +1,2 @@
+def train(configs):
+    NotImplementedError
--- pretrain.py
+++ pretrain.py
@@ -1,26 +1,20 @@
 import albert.configs as configs
-import numpy as np
+from albert.bin.run_pretrain import train
 
 from albert.utils import ArgumentParser
-from albert.models.model import AlbertModel
 
-def train(opt):
-    albert = AlbertModel(opt)
-    print(albert)
-    params = sum([np.prod(p.size()) for p in albert.parameters()])
-    print(params)
 
 def _get_parser():
     parser = ArgumentParser(description='train.py')
     configs.model_opts(parser)
-
+    configs.pretrain_opts(parser)
     return parser
 
 
 def main():
     parser = _get_parser()
-    opt = parser.parse_args()
-    train(opt)
+    conf = parser.parse_args()
+    train(conf)
 
 
 if __name__ == "__main__":
